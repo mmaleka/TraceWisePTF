07d02f4943bf8eaafb2cf5397d4406fc7b4a14dd
Mpho <mpho.maleka3@gmail.com>
2025-07-17 22:15:06 +0200

    backend v1 fixed pillow

--- api/banding/urls.py
+++ api/banding/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('', BandingListCreateView.as_view(), name='banding-list-create'),
     path('<int:pk>/', BandingRetrieveUpdateDestroyView.as_view(), name='banding-detail'),
 ]
+
+# GET	/banding/	List all banding records
+# POST	/banding/	Create a new banding record
+# GET	/banding/<id>/	Retrieve a specific record by ID
+# PUT	/banding/<id>/	Update a banding record
+# PATCH	/banding/<id>/	Partially update a banding record
+# DELETE	/banding/<id>/	Delete a banding record
